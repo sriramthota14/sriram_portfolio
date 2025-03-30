bd0d00ca748ed0dddc801d2dac7f7b32cb168925
sri1718 <sriram.thota14@gmail.com>
2025-03-30 12:30:42 -0400

    dev commot

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Welcome to Sri Ram Thota Portfolio App...</h1>
+      <h1>Welcome to Sri Ram Thota Portfolio App from Dev branch...</h1>
     </>
   );
 }
